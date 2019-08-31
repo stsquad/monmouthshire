6c819c4c05a8a24120414cf4eb2e42a4419eb1db
Alex Bennée <alex.bennee@linaro.org>
2019-08-31 11:06:44 +0100

    makeical.py: generate an .ical file

--- makeical.py
+++ makeical.py
@@ -16,10 +16,18 @@
 #
 
 from bs4 import BeautifulSoup
-import requests
 from argparse import ArgumentParser
+from collections import namedtuple
+from dateutil.parser import parse
+from operator import attrgetter
+from icalendar import Calendar, Event
+from datetime import timedelta
+
+import requests
 
 LOCAL_INFO = "https://maps.monmouthshire.gov.uk/localinfo.aspx"
+
+MyBins = namedtuple("MyBins", "Bins, Date")
 
 def parse_arguments():
     """
@@ -29,7 +37,7 @@ def parse_arguments():
     parser = ArgumentParser(description="Create .ical from Local Info.")
     parser.add_argument("uprn", metavar="N", type=int,
                         help="Your Unique Property Reference Number")
-    parser.add_argument("-f, --file", type=str, help="file to output")
+    parser.add_argument("--file", "-f", type=str, help="file to output")
     return parser.parse_args()
 
 def fetch_info_page(uprn):
@@ -44,7 +52,36 @@ if __name__ == "__main__":
     args = parse_arguments()
     soup = fetch_info_page(args.uprn)
     waste_days = soup.select('div.waste.withExtraText')
+    bins = []
+
     for w in waste_days:
         bin_type = w.find("h4").text
         bin_date = w.find("strong").text
-        print ("Next %s on %s" % (bin_type, bin_date))
+        bins.append(MyBins(bin_type, parse(bin_date)))
+
+    bins = sorted(bins, key=attrgetter("Date"))
+
+    # and now we can construct the ical feed
+    cal = Calendar()
+    cal.add('prodid', '-//When the bins should be put out//mxm.dk//')
+    cal.add('version', '2.0')
+
+    # We want to put the bins out the evening before
+    time_before = timedelta(hours=-6)
+    time_duration = timedelta(minutes=30)
+
+    for b in bins:
+        e = Event()
+        start = getattr(b, "Date") + time_before
+        end = start + time_duration
+        e.add('summary', "Put out %s" % (getattr(b, "Bins")))
+        e.add('dtstart', start)
+        e.add('dtend', end)
+        cal.add_component(e)
+
+    if args.file:
+        f = open(args.file, 'wb')
+        f.write(cal.to_ical())
+    else:
+        for b in bins:
+            print ("Put out %s before %s" % (getattr(b, "Bins"), getattr(b, "Date")))
